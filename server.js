5247e4dc9b30192134a24ee791ae90487a2ec49d
sara latorre <saralatorre97@gmail.com>
2023-10-29 00:06:41 +0200

    updated server to restor swagger ui

--- server.js
+++ server.js
@@ -1,33 +1,29 @@
 const express = require("express");
-const cors = require("cors");
+var cors = require("cors");
+const port = process.env.PORT || 3000;
+const app = express();
 const { initDb } = require('./library/connection');
 
-const app = express();
-const port = process.env.PORT || 3000;
-
-// middleware
 app
-  .use(cors())
-  .use(express.json())
-  .use((req, res, next) => {
+.use(cors())
+.use(express.json())
+.use("/", require("./routes"))
+.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", 
-      "Origin, X-Requested-With, Content-Type, Accept, Z-Key");
-    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-headers", 
+    "Origin, X-Requested-With, Content-Type, Accept, Z-Key");
+    res.setHeader("Content-Type", 
+    "application/json");
     next();
-  });
-
-// routes
-app.use("/", require("./routes"));
-
-app.listen(port, () => {
+})
+.listen(port, () => {
     console.log(`Web Server is listening at port ${port}`);
 });
 
 initDb((err) => {
-    if (err) {
-      console.error("Error connecting to the database:", err);
-    } else {
-      console.log("Connected to MongoDB");
-    }
+  if (err) {
+    console.error("Error connecting to the database:", err);
+  } else {
+    console.log("Connected to MongoDB");
+  }
 });
