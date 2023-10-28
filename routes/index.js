dbd056365f6fbc9c18e8365dbb760a30a166b3e6
sara latorre <saralatorre97@gmail.com>
2023-10-29 00:32:24 +0200

    fixed auth0 set up and swagger ui

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,15 @@ const router = express.Router();
 const booksRouter = require("./books");
 const readRouter = require("./read");
 const usersRouter = require("./users");
-
+const { auth } = require("express-openid-connect");
+const auth0Config = require("../config/auth0-config");
 
 const swaggerUi = require("swagger-ui-express");
 const swaggerDoc = require("./swagger.json");
 
 router.use("/books", booksRouter);
 router.use("/read", readRouter);
-router.use("/users", usersRouter);
+router.use("/users", auth(auth0Config), usersRouter)
 
 // swagger UI middleware
 router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,13 @@
 const routes = require("express").Router();
 const usersController = require("../controllers/user");
-const { auth } = require("express-openid-connect");
-const auth0Config = require("../config/auth0-config");
 
 // validator
 const { validate, validateId } = require("../middlewares/validator");
 const  { userSchema } = require("../schema/usersSchema");
 
-// Define authentication routes with 'auth' middleware
-routes.use(auth(auth0Config));
-
-// Authenticated route
 routes.get("/", (req, res) => {
     res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
-
 routes.post("/", validate(userSchema), usersController.registerUser);
 routes.post("/:logged", usersController.loginUser);
 routes.put("/:id", validateId, validate(userSchema), usersController.updateUser);
